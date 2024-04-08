87f0244785f37d57fe34bcccd66dc5d8319c99b2
miguelangelblancoguerrero <miguelangelblgu@ufps.edu.co>
2024-04-08 11:48:11 -0500

    correccion a

--- script.js
+++ script.js
@@ -2,8 +2,20 @@ const form = document.forms['loginForm'];
 
 form.addEventListener('sumbit',function handleFormSubmit(event){
 event.preventDefault();
-
+alert("Hols");
 const email = form['email'].value;
 const password = form['password'].value;
 
-})
+loginUser(email,password);
+});
+
+function loginUser(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      console.log('Credenciales correctas, ¡bienvenido! ' + user);
+      localStorage.setItem('user', JSON.stringify(user));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
